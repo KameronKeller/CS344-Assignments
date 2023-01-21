1847d9be2e1acb21c87573541c44c18caeb88413
Kameron Keller <mrkameronkeller@gmail.com>
2023-01-21 14:08:16 -0800

    Step 1: Shake the rust off

---
+++ project2_writing_utilities/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int x = 12;
+	float y = 3.14;
+	char* s = "Hello, world!";
+
+	printf("x is %d, y is %f\n%s\n", x, y, s);
+
+	for (int i = 0; i < 5; i++) {
+		printf("%d x 5 = %d\n", i, i * 5);
+	}
+
+	return 0;
+}
