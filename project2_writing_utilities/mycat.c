39597d663f14ae76d06595dc14b100aea5784f03
Kameron Keller <mrkameronkeller@gmail.com>
2023-01-21 18:29:10 -0800

    Clean up duplicate code and remove unneeded variable

--- project2_writing_utilities/mycat.c
+++ project2_writing_utilities/mycat.c
@@ -7,25 +7,18 @@ int main(int argc, char const *argv[]) {
 	const int STDIN = 0;
 	const int STDOUT = 1;
 	const int MAX_BUFFER_SIZE = 2048;
-	const int TRUE = 1;
 	const int NO_ARGS = 1;
 
 	char buffer[MAX_BUFFER_SIZE];
+	int bytesRead = 0;
 
 	if (argc == NO_ARGS) {
-		while (TRUE) {
-			int bytesRead = read(STDIN, &buffer, MAX_BUFFER_SIZE);
-			// EOF will have 0 bytes, an error will return -1
-			if (bytesRead <= 0) {
-				break;
-			} else {
-				write(STDOUT, &buffer, bytesRead);
-			}
+		while ((bytesRead = read(STDIN, &buffer, MAX_BUFFER_SIZE)) != 0) {
+			write(STDOUT, &buffer, bytesRead);
 		}
 	} else {
 		for (int i = 1; i < argc; i++) {
 			int fildes = open(argv[i], O_RDONLY);
-			int bytesRead = 0;
 
 			if (fildes == -1) {
 				perror("open");
--- project2_writing_utilities/yell.c
+++ project2_writing_utilities/yell.c
@@ -4,33 +4,30 @@
 #include <stdio.h> //perror()
 #include <stdlib.h> //exit()
 
+void convert_buffer(int bytesRead, char *buffer) {
+	for (int i = 0; i < bytesRead; i++) {
+		buffer[i] = toupper(buffer[i]);
+	}
+}
+
 int main(int argc, char const *argv[]) {
 	const int STDIN = 0;
 	const int STDOUT = 1;
 	const int MAX_BUFFER_SIZE = 2048;
-	const int TRUE = 1;
 	const int NO_ARGS = 1;
 
 	char buffer[MAX_BUFFER_SIZE];
+	int bytesRead = 0;
+
 
 	if (argc == NO_ARGS) {
-		while (TRUE) {
-			int bytesRead = read(STDIN, &buffer, MAX_BUFFER_SIZE);
-			// EOF will have 0 bytes, an error will return -1
-			if (bytesRead <= 0) {
-				break;
-			} else {
-				for (int i = 0; i < bytesRead; i++) {
-					buffer[i] = toupper(buffer[i]);
-				}
-
-				write(STDOUT, &buffer, bytesRead);
-			}
+		while ((bytesRead = read(STDIN, &buffer, MAX_BUFFER_SIZE)) != 0) {
+			convert_buffer(bytesRead, buffer);
+			write(STDOUT, &buffer, bytesRead);
 		}
 	} else {
 		for (int i = 1; i < argc; i++) {
 			int fildes = open(argv[i], O_RDONLY);
-			int bytesRead = 0;
 
 			if (fildes == -1) {
 				perror("open");
@@ -38,9 +35,7 @@ int main(int argc, char const *argv[]) {
 			}
 
 			while ((bytesRead = read(fildes, &buffer, MAX_BUFFER_SIZE)) != 0) {
-				for (int i = 0; i < bytesRead; i++) {
-					buffer[i] = toupper(buffer[i]);
-				}
+				convert_buffer(bytesRead, buffer);
 				write(STDOUT, &buffer, bytesRead);
 			}
 			close(fildes);
