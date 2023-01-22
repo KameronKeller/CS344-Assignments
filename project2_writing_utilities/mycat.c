33df26ec93b1a9505b90ffd57c6024f519fa9bc9
Kameron Keller <mrkameronkeller@gmail.com>
2023-01-21 18:19:02 -0800

    Refactor to include while loop

--- project2_writing_utilities/mycat.c
+++ project2_writing_utilities/mycat.c
@@ -1,18 +1,20 @@
 #include <fcntl.h> // open()
 #include <unistd.h> // read(), write(), close()
+#include <stdio.h> //perror()
+#include <stdlib.h> //exit()
 
 int main(int argc, char const *argv[]) {
 	const int STDIN = 0;
 	const int STDOUT = 1;
-	const int MAX_FILE_SIZE = 2048;
+	const int MAX_BUFFER_SIZE = 2048;
 	const int TRUE = 1;
 	const int NO_ARGS = 1;
 
-	char buffer[MAX_FILE_SIZE];
+	char buffer[MAX_BUFFER_SIZE];
 
 	if (argc == NO_ARGS) {
 		while (TRUE) {
-			int bytesRead = read(STDIN, &buffer, MAX_FILE_SIZE);
+			int bytesRead = read(STDIN, &buffer, MAX_BUFFER_SIZE);
 			// EOF will have 0 bytes, an error will return -1
 			if (bytesRead <= 0) {
 				break;
@@ -23,8 +25,16 @@ int main(int argc, char const *argv[]) {
 	} else {
 		for (int i = 1; i < argc; i++) {
 			int fildes = open(argv[i], O_RDONLY);
-			int bytesRead = read(fildes, &buffer, MAX_FILE_SIZE);
-			write(STDOUT, &buffer, bytesRead);
+			int bytesRead = 0;
+
+			if (fildes == -1) {
+				perror("open");
+				exit(1);
+			}
+
+			while ((bytesRead = read(fildes, &buffer, MAX_BUFFER_SIZE)) != 0) {
+				write(STDOUT, &buffer, bytesRead);
+			}
 			close(fildes);
 		}
 	}
