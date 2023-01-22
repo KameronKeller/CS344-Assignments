ba396a266953697c55a1f32fc13d03b233a36516
Kameron Keller <mrkameronkeller@gmail.com>
2023-01-21 16:51:43 -0800

    Rename variables for consistency

--- project2_writing_utilities/readfile.c
+++ project2_writing_utilities/readfile.c
@@ -4,14 +4,14 @@
 int main(int argc, char const *argv[]) {
 	const int MAX_FILE_SIZE = 2048;
 	
-	int stdout = 1;
+	const int STDOUT = 1;
 	char buffer[MAX_FILE_SIZE];
 
 	int fildes = open(argv[1], O_RDONLY);
 	// Note to self:	read() expects a pointer to a buffer,
 	//					so must use address-of operator
-	int bytes_read = read(fildes, &buffer, MAX_FILE_SIZE);
-	write(stdout, &buffer, bytes_read);
+	int bytesRead = read(fildes, &buffer, MAX_FILE_SIZE);
+	write(STDOUT, &buffer, bytesRead);
 	close(fildes);
 	return 0;
 }
