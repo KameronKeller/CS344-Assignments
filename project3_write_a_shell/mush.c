db9f5a497ef036f56b0b15b3fcd7cbeb3b0a42a7
Kameron Keller <mrkameronkeller@gmail.com>
2023-01-25 11:00:28 -0800

    Include headers and create basic structure

---
+++ project3_write_a_shell/mush.c
@@ -0,0 +1,19 @@
+#include <stdio.h> // for printf(), fgets(), perror()
+#include <stdlib.h> // for exit()
+#include <string.h> // for strtok(), strcmp()
+#include <unistd.h> // for fork(), execvp()
+#include <sys/wait.h> // for wait()
+
+int main(int argc, char const *argv[]) {
+
+    (void)argc;  // Expression does nothing, but now argc is "used"
+    (void)argv;
+
+
+
+
+
+
+
+    return 0;
+}
