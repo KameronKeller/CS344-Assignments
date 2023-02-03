df119e4c35753808dfad0e1c91b24c9bcd1dd7b5
Kameron Keller <mrkameronkeller@gmail.com>
2023-02-02 20:16:04 -0800

    Create word count and list files functions

--- project4_pipes/lswc.c
+++ project4_pipes/lswc.c
@@ -2,12 +2,15 @@
 #include <stdlib.h> // for exit()
 #include <unistd.h> // for fork(), execlp(), close()
 
+#define STDIN 0
+#define STDOUT 1
+
+
 int no_arguments(int argc) {
-    return argc > 1? 0 : 1; // return 0 if no additional arguments
+    return argc > 1 ? 0 : 1; // return 0 if no additional arguments
 }
 
-const char *parse_input(int argc, const char *argv[]) {
-// char *parse_input(int argc) {
+char *parse_input(int argc, char *argv[]) {
     if (no_arguments(argc)) {
         return ".";
     } else {
@@ -15,12 +18,36 @@ const char *parse_input(int argc, const char *argv[]) {
     }
 }
 
+int is_child_process(int pid) {
+    return pid == 0 ? 1 : 0;
+}
 
-int main(int argc, const char *argv[]) {
+void count_files(int pipe_fd[]) {
+    const int READ_PIPE = pipe_fd[0];
+    const int WRITE_PIPE = pipe_fd[1];
+
+    // dup2(STDIN, READ_PIPE); // original
+    dup2(READ_PIPE, STDIN);
+    close(WRITE_PIPE);
+    execlp("wc", "wc", "-l", NULL);
+}
+
+void list_files(int pipe_fd[], char *path) {
+    const int READ_PIPE = pipe_fd[0];
+    const int WRITE_PIPE = pipe_fd[1];
+
+    // dup2(STDOUT, WRITE_PIPE); // original
+    dup2(WRITE_PIPE, STDOUT);
+    close(READ_PIPE);
+    execlp("ls","ls", "-1a", path, NULL);
+
+}
+
+int main(int argc, char *argv[]) {
     int pipe_fd[2];
 
 
-    const char *path = parse_input(argc, argv);
+    char *path = parse_input(argc, argv);
     // printf("%s\n", path);
     // create_pipe()
     int pipe_status = pipe(pipe_fd);
@@ -28,10 +55,12 @@ int main(int argc, const char *argv[]) {
         perror("pipe failure");
 
     pid_t pid = fork();
-    // if child_process(pid) {}
-    // else {
-        // wait(NULL);
-    // }  
+    if (is_child_process(pid)) {
+        count_files(pipe_fd);
+    } else {
+        list_files(pipe_fd, path);
+        wait(NULL);
+    }  
 
     return 0;
 }
