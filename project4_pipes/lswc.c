d26df9779b3e185324714be68b0b7033751c2b22
Kameron Keller <mrkameronkeller@gmail.com>
2023-02-02 16:16:47 -0800

    initial commit

---
+++ project4_pipes/lswc.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // for printf(), perror()
+#include <stdlib.h> // for exit()
+#include <unistd.h> // for fork(), execlp(), close()
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
