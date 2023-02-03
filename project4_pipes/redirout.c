554f7684d683da3695b72210df29f7d3ca7ddf68
Kameron Keller <mrkameronkeller@gmail.com>
2023-02-03 10:47:38 -0800

    initial commit

---
+++ project4_pipes/redirout.c
@@ -0,0 +1,22 @@
+/*
+Author: Kameron Keller
+Course: CS 344 OS I
+Assignment: Project 4
+Example usage:
+
+    $ redirout foo.txt ls -l -a
+
+*/
+
+
+#include <stdio.h> // for printf(), perror()
+#include <stdlib.h> // for exit()
+#include <unistd.h> // for fork(), execlp(), close()
+#include <fcntl.h> // for open()
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
