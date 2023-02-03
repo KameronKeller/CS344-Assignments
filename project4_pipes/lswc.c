5f1af2c861d334556f26a5e5bf7d94f86d4ff2e5
Kameron Keller <mrkameronkeller@gmail.com>
2023-02-02 16:29:34 -0800

    Sketch outline

--- project4_pipes/lswc.c
+++ project4_pipes/lswc.c
@@ -2,8 +2,16 @@
 #include <stdlib.h> // for exit()
 #include <unistd.h> // for fork(), execlp(), close()
 
-int main(int argc, char const *argv[])
-{
-    /* code */
+int main(int argc, char const *argv[]) {
+
+    // parse_input(argc, argv);
+    // create_pipe()
+    // pid_t pid = fork()
+    // if child_process(pid) {}
+    // else {
+        // wait(NULL);
+    // }  
+
     return 0;
 }
+
