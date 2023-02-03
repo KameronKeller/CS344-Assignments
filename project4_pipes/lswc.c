3a0aaf3248a540676eabccc8e89de8f07dfb06c3
Kameron Keller <mrkameronkeller@gmail.com>
2023-02-02 16:51:11 -0800

    Parse input function

--- project4_pipes/lswc.c
+++ project4_pipes/lswc.c
@@ -2,9 +2,26 @@
 #include <stdlib.h> // for exit()
 #include <unistd.h> // for fork(), execlp(), close()
 
-int main(int argc, char const *argv[]) {
+int no_arguments(int argc) {
+    return argc > 1? 0 : 1; // return 0 if no additional arguments
+}
 
-    // parse_input(argc, argv);
+const char *parse_input(int argc, const char *argv[]) {
+// char *parse_input(int argc) {
+    if (no_arguments(argc)) {
+        return ".";
+    } else {
+        return argv[1];
+    }
+}
+
+
+int main(int argc, const char *argv[]) {
+
+
+
+    const char *path = parse_input(argc, argv);
+    // printf("%s\n", path);
     // create_pipe()
     // pid_t pid = fork()
     // if child_process(pid) {}
